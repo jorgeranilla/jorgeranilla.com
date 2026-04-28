811f77dad2a3457b985c6a3dcc61d4d07b7ef45b
Jorge Ranilla <jorgeranilla@gmail.com>
2026-04-27 22:29:38 -0400

    new

--- script.js
+++ script.js
@@ -274,11 +274,11 @@ document.addEventListener("DOMContentLoaded", () => {
               </button>
 
               <!-- Family Members Login (Lock Icon) -->
-              <a href="${familyLinks.familyLogin}" class="search-icon-btn" aria-label="Family Members Login" title="Family Members Login">
+              <button class="search-icon-btn" aria-label="Family Members Login" title="Family Members Login" onclick="window.location.href='${familyLinks.familyLogin}'">
                 <svg viewBox="0 0 24 24" width="20" height="20">
                   <path fill="currentColor" d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9H8.9V6c0-1.71 1.39-3.1 3.1-3.1 1.71 0 3.1 1.39 3.1 3.1v2z"/>
                 </svg>
-              </a>
+              </button>
 
           </div>
         </nav>
