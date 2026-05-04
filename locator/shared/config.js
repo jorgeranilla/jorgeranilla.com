f23cf53dfe2bdf2af2c0223d51fd8c3c7d477359
Jorge Ranilla <jorgeranilla@gmail.com>
2026-05-04 10:41:36 -0400

    new

---
+++ locator/shared/config.js
@@ -0,0 +1,15 @@
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyBc0zrXLzr9Qhq9iuhXLAXOzbkQ13mEaU4',
+  authDomain: 'jorgeranilla-site.firebaseapp.com',
+  databaseURL: 'https://jorgeranilla-site-default-rtdb.firebaseio.com',
+  projectId: 'jorgeranilla-site',
+  storageBucket: 'jorgeranilla-site.firebasestorage.app',
+  messagingSenderId: '125483521813',
+  appId: '1:125483521813:web:f48b02d491cb4c698ffb1c',
+};
+
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyDweQvQGRHaUMdfYNa7lWA3dUrJYxPhfH8';
+
+export const ADMIN_UID = '5CYFFoj3JCShd9kDjUBESbJjRVY2';
+
+export const LOCATION_PATH = 'publicLocation/jorge';
